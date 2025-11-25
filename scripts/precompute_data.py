a2997b4c5b44eec10d7165949df6fee7433d15c3
Oliver Oayda <oliver.oayda@sydney.edu.au>
2025-11-25 17:36:30 +1100

    build: add new unmasked covmap data

---
+++ scripts/precompute_data.py
@@ -0,0 +1,12 @@
+from catsim import CatwiseConfig, Catwise
+
+
+config = CatwiseConfig(
+    cat_w12_min=0.5, 
+    cat_w1_max=17.0, 
+    magnitude_error_dist='gaussian'
+)
+sim = Catwise(config)
+
+sim.load_catalogue()
+sim.create_coverage_maps(use_mask=False)
