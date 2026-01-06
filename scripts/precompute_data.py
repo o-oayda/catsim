860430c678b863489d819253552fc58278e602af
Oliver Oayda <oaydaoliver@gmail.com>
2026-01-07 10:31:58 +1100

    update precompute data script

--- scripts/precompute_data.py
+++ scripts/precompute_data.py
@@ -8,9 +8,15 @@ config = CatwiseConfig(
 )
 sim = Catwise(config)
 
-sim.load_catalogue()
-# sim.create_coverage_maps(use_mask=False)
-sim.determine_masked_pixels()
-sim.make_masked_catalogue()
-# sim.create_magnitude_coverage_cell_dist()
-sim.create_mag_cov_hashgrid()
+sim.precompute_data()
+
+# if False:
+# sim.load_catalogue()
+# # sim.create_coverage_maps(use_mask=False)
+# sim.determine_masked_pixels()
+# sim.make_masked_catalogue()
+# # sim.create_magnitude_coverage_cell_dist()
+# sim.create_mag_cov_hashgrid(
+#     grid_step=[0.1, 0.02, 0.1, 0.04], 
+#     project_coverage=True
+# )
